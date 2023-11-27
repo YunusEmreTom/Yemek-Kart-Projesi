2a59ab2b91583b0bfec37af872f0c15103a40706
YunusEmreTom <78315933+YunusEmreTom@users.noreply.github.com>
2023-11-27 18:19:41 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,145 +1,98 @@
 #include <SPI.h>
-#include <RFID.h>
+#include <MFRC522.h>
+#include <SD.h>
 
-RFID rfid(10,5);    //D10--读卡器MOSI引脚、D5--读卡器RST引脚
+#define SS_PIN 10 // RFID modülü için kullanılan slave select pin
+#define RST_PIN 9 // RFID modülü için kullanılan reset pin
+#define RED_LED 5  // Kırmızı LED pin
+#define GREEN_LED 6  // Yeşil LED pin
 
-#define KiLed           5
-#define YeLed           6
-#define Buzzer          7
+MFRC522 mfrc522(SS_PIN, RST_PIN); // RFID nesnesi oluştur
 
-unsigned char serNum[5];
-//写卡数据
-unsigned char writeDate[16] ={'G', 'e', 'e', 'k', '-', 'W', 'o', 'r', 'k', 'S', 'h', 'o', 'p', 0, 0, 0};
-//原扇区A密码，16个扇区，每个扇区密码6Byte
-unsigned char sectorKeyA[16][16] = {
-        {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF},
-        {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF},
-        {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF},};
-//新扇区A密码，16个扇区，每个扇区密码6Byte
-unsigned char sectorNewKeyA[16][16] = {
-        {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF},
-        {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xff,0x07,0x80,0x69, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF},
-        {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xff,0x07,0x80,0x69, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF},};
+File myFile; // SD kart dosya nesnesi oluştur
 
+void setup() {
+  Serial.begin(9600); // Seri haberleşmeyi başlat
+  SPI.begin(); // SPI haberleşmesini başlat
+  mfrc522.PCD_Init(); // RFID modülünü başlat
 
-String RF_Write(){
-  unsigned char i,tmp;
-  unsigned char status;
-  unsigned char str[MAX_LEN];
-  unsigned char RC_size;
-  unsigned char blockAddr;        
-  
-  if (rfid.readCardSerial())
-  {
-    Serial.print("The card's number is  : ");
-    Serial.print(rfid.serNum[0],HEX);
-    Serial.print(rfid.serNum[1],HEX);
-    Serial.print(rfid.serNum[2],HEX);
-    Serial.print(rfid.serNum[3],HEX);
-    Serial.print(rfid.serNum[4],HEX);
-    Serial.println(" ");
+  pinMode(RED_LED, OUTPUT); // Kırmızı LED'i çıkış olarak ayarla
+  pinMode(GREEN_LED, OUTPUT); // Yeşil LED'i çıkış olarak ayarla
+
+  if (!SD.begin(4)) { // SD kartı başlat, 4. pin slave select pinidir
+    Serial.println("SD kart başlatılamadı!");
+    return;
   }
+  Serial.println("SD kart başarıyla başlatıldı.");
+}
 
-  //选卡，返回卡容量（锁定卡片，防止多次读写）
-  rfid.selectTag(rfid.serNum);
-  
-  //写数据卡
-  blockAddr = 7;                //数据块7
-  if (rfid.auth(PICC_AUTHENT1A, blockAddr, sectorKeyA[blockAddr/4], rfid.serNum) == MI_OK)  //认证
-  {
-    //写数据
-    status = rfid.write(blockAddr, sectorNewKeyA[blockAddr/4]);
-    Serial.print("set the new card password, and can modify the data of the Sector: ");
-    Serial.println(blockAddr/4,DEC);
-    //写数据
-    blockAddr = blockAddr - 3 ; //数据块4
-    status = rfid.write(blockAddr, writeDate);
-    if(status == MI_OK)
-    {
-      Serial.println("Write card OK!");
+void loop() {
+  if (mfrc522.PICC_IsNewCardPresent() && mfrc522.PICC_ReadCardSerial()) {
+    String cardNumber = "";
+    for (byte i = 0; i < mfrc522.uid.size; i++) {
+      cardNumber += String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
+      cardNumber += String(mfrc522.uid.uidByte[i], HEX);
     }
+    cardNumber.toUpperCase();
+    
+    Serial.println("Okunan kart numarası: " + cardNumber);
+    
+    int remainingMeals = getRemainingMeals(cardNumber);
+
+    if (remainingMeals > 0) {
+      digitalWrite(GREEN_LED, HIGH); // Yeşil LED'i yak
+      digitalWrite(RED_LED, LOW); // Kırmızı LED'i söndür
+      
+      Serial.println("Yemek hakkınız var. Kalan hak: " + String(remainingMeals));
+      
+      updateRemainingMeals(cardNumber, remainingMeals - 1); // Yemek hakkını bir azalt
+      
+      logCardNumber(cardNumber); // Kart numarasını SD karta kaydet
+    } else {
+      digitalWrite(GREEN_LED, LOW); // Yeşil LED'i söndür
+      digitalWrite(RED_LED, HIGH); // Kırmızı LED'i yak
+      
+      Serial.println("Yemek hakkınız bitmiştir. Yemek yiyemezsiniz.");
+    }
+
+    delay(2000); // Kartın sürekli okunmasını önlemek için kısa bir bekleme
   }
 }
 
-String RF_Read(){
-  unsigned char i,tmp;
-  unsigned char status;
-  unsigned char str[MAX_LEN];
-  unsigned char RC_size;
-  String KartID;
-
-  unsigned char blockAddr;        
-
-  rfid.selectTag(rfid.serNum);
-  blockAddr = 7;                //数据块7
-  status = rfid.auth(PICC_AUTHENT1A, blockAddr, sectorNewKeyA[blockAddr/4], rfid.serNum);
-  if (status == MI_OK)  //认证
-  {
-    //读数据
-    blockAddr = blockAddr - 3 ; //数据块4
-    if( rfid.read(blockAddr, str) == MI_OK)
-    {
-      Serial.print("Read from the card ,the data is : ");
-      Serial.println((char *)str);
-      KartID=(char *)str;
-
-    }
-  }
+int getRemainingMeals(String cardNumber) {
+  // Bu fonksiyon, kart numarasına göre kalan yemek hakkını SD karttan okur
+  String fileName = "/YemekHaklari/" + cardNumber + ".txt";
   
-  return KartID;
-}
-void Gec(){
-  digitalWrite(YeLed,HIGH);
-  digitalWrite(Buzzer,HIGH);
-  delay(300);
-  digitalWrite(YeLed,LOW);
-  digitalWrite(Buzzer,LOW);
-}
-void Dur(){
-  digitalWrite(KiLed,HIGH);
-  digitalWrite(Buzzer,HIGH);
-  delay(150);
-  digitalWrite(Buzzer,LOW);
-  delay(150);
-  digitalWrite(Buzzer,HIGH);
-  delay(150);
-  digitalWrite(Buzzer,LOW);
-  delay(150);
-  digitalWrite(Buzzer,HIGH);
-  delay(150);
-  digitalWrite(Buzzer,LOW);
-  digitalWrite(KiLed,LOW);
-}
-void setup()
-{
-  Serial.begin(9600);
-  SPI.begin();
-  rfid.init();
-  pinMode(YeLed,OUTPUT);
-  pinMode(KiLed,OUTPUT);
-  pinMode(Buzzer,OUTPUT);
+  if (SD.exists(fileName)) {
+    myFile = SD.open(fileName, FILE_READ);
+    int remainingMeals = myFile.parseInt();
+    myFile.close();
+    
+    return remainingMeals;
+  } else {
+    // Eğer dosya yoksa, yeni bir dosya oluşturup maksimum yemek hakkını ayarla
+    myFile = SD.open(fileName, FILE_WRITE);
+    myFile.println("5"); // Örneğin, başlangıçta herkese 5 yemek hakkı verilmiştir
+    myFile.close();
+    
+    return 5;
+  }
 }
 
-void loop()
-{
-  rfid.isCard();
-  String newKartID = RF_Read();
-  String lastTwoDigits = newKartID.substring(13 - 2);
-  int lastTwoDigitsInt = lastTwoDigits.toInt();
-  if (lastTwoDigitsInt>0){
-    lastTwoDigitsInt--;
-    String newString = newKartID.substring(0, 13 - 2) + String(lastTwoDigitsInt);
-    for (int i = 0; i < newString.length() && i < sizeof(writeDate); i++) {
-      writeDate[i] = static_cast<unsigned char>(newString.charAt(i));
-    }
-    RF_Write();
-    Serial.println("geç");
-    Gec();
-    delay(500);
-  }
-  else{
-    Serial.println("dur");
-    Dur();
-  }
-  rfid.halt();
+void updateRemainingMeals(String cardNumber, int remainingMeals) {
+  // Bu fonksiyon, kart numarasına göre kalan yemek hakkını SD kartta günceller
+  String fileName = "/YemekHaklari/" + cardNumber + ".txt";
+  
+  myFile = SD.open(fileName, FILE_WRITE);
+  myFile.println(remainingMeals);
+  myFile.close();
+}
+
+void logCardNumber(String cardNumber) {
+  // Bu fonksiyon, o gün yemek yiyen kişinin kart numarasını SD karta kaydeder
+  String fileName = "/GunlukYemekListesi/gunluk_liste.txt";
+  
+  myFile = SD.open(fileName, FILE_WRITE);
+  myFile.println(cardNumber);
+  myFile.close();
 }
