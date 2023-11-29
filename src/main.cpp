a70764f9d8d2f0061ff12a0dfb50ae79818b7f7d
YunusEmreTom <78315933+YunusEmreTom@users.noreply.github.com>
2023-11-29 17:40:48 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,98 +1,195 @@
 #include <SPI.h>
 #include <MFRC522.h>
-#include <SD.h>
 
-#define SS_PIN 10 // RFID modülü için kullanılan slave select pin
-#define RST_PIN 9 // RFID modülü için kullanılan reset pin
-#define RED_LED 5  // Kırmızı LED pin
-#define GREEN_LED 6  // Yeşil LED pin
+/*Using Hardware SPI of Arduino */
+/*MOSI (11), MISO (12) and SCK (13) are fixed */
+/*You can configure SS and RST Pins*/
+#define SS_PIN 10  /* Slave Select Pin */
+#define RST_PIN 7  /* Reset Pin */
 
-MFRC522 mfrc522(SS_PIN, RST_PIN); // RFID nesnesi oluştur
+/* Create an instance of MFRC522 */
+MFRC522 mfrc522(SS_PIN, RST_PIN);
+/* Create an instance of MIFARE_Key */
+MFRC522::MIFARE_Key key;          
 
-File myFile; // SD kart dosya nesnesi oluştur
+/* Set the block to which we want to write data */
+/* Be aware of Sector Trailer Blocks */
+int blockNum = 2;  
+/* Create an array of 16 Bytes and fill it with data */
+/* This is the actual data which is going to be written into the card */
+byte blockData [16] = {"Electronics-Hub-"};
 
-void setup() {
-  Serial.begin(9600); // Seri haberleşmeyi başlat
-  SPI.begin(); // SPI haberleşmesini başlat
-  mfrc522.PCD_Init(); // RFID modülünü başlat
+/* Create another array to read data from Block */
+/* Legthn of buffer should be 2 Bytes more than the size of Block (16 Bytes) */
+byte bufferLen = 18;
+byte readBlockData[18];
 
-  pinMode(RED_LED, OUTPUT); // Kırmızı LED'i çıkış olarak ayarla
-  pinMode(GREEN_LED, OUTPUT); // Yeşil LED'i çıkış olarak ayarla
+MFRC522::StatusCode status;
 
-  if (!SD.begin(4)) { // SD kartı başlat, 4. pin slave select pinidir
-    Serial.println("SD kart başlatılamadı!");
+void setup() 
+{
+  /* Initialize serial communications with the PC */
+  Serial.begin(9600);
+  /* Initialize SPI bus */
+  SPI.begin();
+  /* Initialize MFRC522 Module */
+  mfrc522.PCD_Init();
+  Serial.println("Scan a MIFARE 1K Tag to write data...");
+}
+
+void loop()
+{
+  /* Prepare the ksy for authentication */
+  /* All keys are set to FFFFFFFFFFFFh at chip delivery from the factory */
+  for (byte i = 0; i < 6; i++)
+  {
+    key.keyByte[i] = 0xFF;
+  }
+  /* Look for new cards */
+  /* Reset the loop if no new card is present on RC522 Reader */
+  if ( ! mfrc522.PICC_IsNewCardPresent())
+  {
     return;
   }
-  Serial.println("SD kart başarıyla başlatıldı.");
-}
-
-void loop() {
-  if (mfrc522.PICC_IsNewCardPresent() && mfrc522.PICC_ReadCardSerial()) {
-    String cardNumber = "";
-    for (byte i = 0; i < mfrc522.uid.size; i++) {
-      cardNumber += String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
-      cardNumber += String(mfrc522.uid.uidByte[i], HEX);
-    }
-    cardNumber.toUpperCase();
-    
-    Serial.println("Okunan kart numarası: " + cardNumber);
-    
-    int remainingMeals = getRemainingMeals(cardNumber);
-
-    if (remainingMeals > 0) {
-      digitalWrite(GREEN_LED, HIGH); // Yeşil LED'i yak
-      digitalWrite(RED_LED, LOW); // Kırmızı LED'i söndür
-      
-      Serial.println("Yemek hakkınız var. Kalan hak: " + String(remainingMeals));
-      
-      updateRemainingMeals(cardNumber, remainingMeals - 1); // Yemek hakkını bir azalt
-      
-      logCardNumber(cardNumber); // Kart numarasını SD karta kaydet
-    } else {
-      digitalWrite(GREEN_LED, LOW); // Yeşil LED'i söndür
-      digitalWrite(RED_LED, HIGH); // Kırmızı LED'i yak
-      
-      Serial.println("Yemek hakkınız bitmiştir. Yemek yiyemezsiniz.");
-    }
-
-    delay(2000); // Kartın sürekli okunmasını önlemek için kısa bir bekleme
+  
+  /* Select one of the cards */
+  if ( ! mfrc522.PICC_ReadCardSerial()) 
+  {
+    return;
   }
-}
-
-int getRemainingMeals(String cardNumber) {
-  // Bu fonksiyon, kart numarasına göre kalan yemek hakkını SD karttan okur
-  String fileName = "/YemekHaklari/" + cardNumber + ".txt";
-  
-  if (SD.exists(fileName)) {
-    myFile = SD.open(fileName, FILE_READ);
-    int remainingMeals = myFile.parseInt();
-    myFile.close();
-    
-    return remainingMeals;
-  } else {
-    // Eğer dosya yoksa, yeni bir dosya oluşturup maksimum yemek hakkını ayarla
-    myFile = SD.open(fileName, FILE_WRITE);
-    myFile.println("5"); // Örneğin, başlangıçta herkese 5 yemek hakkı verilmiştir
-    myFile.close();
-    
-    return 5;
+  Serial.print("\n");
+  Serial.println("**Card Detected**");
+  /* Print UID of the Card */
+  Serial.print(F("Card UID:"));
+  for (byte i = 0; i < mfrc522.uid.size; i++)
+  {
+    Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
+    Serial.print(mfrc522.uid.uidByte[i], HEX);
   }
+  Serial.print("\n");
+  /* Print type of card (for example, MIFARE 1K) */
+  Serial.print(F("PICC type: "));
+  MFRC522::PICC_Type piccType = mfrc522.PICC_GetType(mfrc522.uid.sak);
+  Serial.println(mfrc522.PICC_GetTypeName(piccType));
+
+  Serial.print("\n");
+  Serial.println("Reading from Data Block...");
+  ReadDataFromBlock(blockNum, readBlockData);
+  
+  /* If you want to print the full memory dump, uncomment the next line */
+  //mfrc522.PICC_DumpToSerial(&(mfrc522.uid));
+  
+  /* Print the data read from block */
+  Serial.print("\n");
+  Serial.print("Data in Block:");
+  Serial.print(blockNum);
+  Serial.print(" --> ");
+  for (int j=0 ; j<16 ; j++)
+  {
+    Serial.write(readBlockData[j]);
+  }      
+  int kontrol =  decreaseNumbers(readBlockData, 2);
+  if (kontrol !=0){
+    WriteDataToBlock(readBlockData, blockData);
+  }
+  else{
+    int kontrol =  decreaseNumbers(readBlockData, 1);
+    if (kontrol != 0){
+      WriteDataToBlock(readBlockData, blockData);
+    }
+    else{
+      Serial.println("geçişe izin yoktur.")
+    }
+  }
+  /* Call 'WriteDataToBlock' function, which will write data to the block */
+  Serial.print("\n");
+  Serial.println("Writing to Data Block...");
+  WriteDataToBlock(readBlockData, blockData);
+  
+  /* Read data from the same block */
+  
+  Serial.print("\n");
 }
 
-void updateRemainingMeals(String cardNumber, int remainingMeals) {
-  // Bu fonksiyon, kart numarasına göre kalan yemek hakkını SD kartta günceller
-  String fileName = "/YemekHaklari/" + cardNumber + ".txt";
+
+
+void WriteDataToBlock(int blockNum, byte blockData[]) 
+{
+  /* Authenticating the desired data block for write access using Key A */
+  status = mfrc522.PCD_Authenticate(MFRC522::PICC_CMD_MF_AUTH_KEY_A, blockNum, &key, &(mfrc522.uid));
+  if (status != MFRC522::STATUS_OK)
+  {
+    Serial.print("Authentication failed for Write: ");
+    Serial.println(mfrc522.GetStatusCodeName(status));
+    return;
+  }
+  else
+  {
+    Serial.println("Authentication success");
+  }
+
   
-  myFile = SD.open(fileName, FILE_WRITE);
-  myFile.println(remainingMeals);
-  myFile.close();
+  /* Write data to the block */
+  status = mfrc522.MIFARE_Write(blockNum, blockData, 16);
+  if (status != MFRC522::STATUS_OK)
+  {
+    Serial.print("Writing to Block failed: ");
+    Serial.println(mfrc522.GetStatusCodeName(status));
+    return;
+  }
+  else
+  {
+    Serial.println("Data was written into Block successfully");
+  }
+  
 }
 
-void logCardNumber(String cardNumber) {
-  // Bu fonksiyon, o gün yemek yiyen kişinin kart numarasını SD karta kaydeder
-  String fileName = "/GunlukYemekListesi/gunluk_liste.txt";
+void ReadDataFromBlock(int blockNum, byte readBlockData[]) 
+{
+  /* Authenticating the desired data block for Read access using Key A */
+  byte status = mfrc522.PCD_Authenticate(MFRC522::PICC_CMD_MF_AUTH_KEY_A, blockNum, &key, &(mfrc522.uid));
+
+  if (status != MFRC522::STATUS_OK)
+  {
+     Serial.print("Authentication failed for Read: ");
+     Serial.println(mfrc522.GetStatusCodeName(status));
+     return;
+  }
+  else
+  {
+    Serial.println("Authentication success");
+  }
+
+  /* Reading data from the Block */
+  status = mfrc522.MIFARE_Read(blockNum, readBlockData, &bufferLen);
+  if (status != MFRC522::STATUS_OK)
+  {
+    Serial.print("Reading failed: ");
+    Serial.println(mfrc522.GetStatusCodeName(status));
+    return;
+  }
+  else
+  {
+    Serial.println("Block was read successfully");  
+  }
   
-  myFile = SD.open(fileName, FILE_WRITE);
-  myFile.println(cardNumber);
-  myFile.close();
+}
+
+
+
+int decreaseNumbers(byte data[], int eleman) {
+    if (isdigit(data[eleman])) {
+      // Eğer eleman bir rakamsa, char olarak al, bir azalt, sonra byte olarak geri çevir
+      char digitChar = data[eleman];
+      int digit = digitChar - '0';
+      
+      if (digit > 0) {
+        digit--;
+        data[eleman] = digit + '0';
+      }
+      else{
+        return 0;
+      }
+    }
+    return 1;
 }
